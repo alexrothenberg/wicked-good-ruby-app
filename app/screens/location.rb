3d8186d144883efdd419225816df85ce82515bc4
Alex Rothenberg <alex@alexrothenberg.com>
2013-09-23 00:16:20 -0400

    some location screen

--- app/screens/location.rb
+++ app/screens/location.rb
@@ -1,5 +1,16 @@
 module Screen
   class Location < PM::Screen
     title 'Location'
+
+    stylesheet :location_styles
+
+    def will_present
+      layout(view, :main_view) do
+        subview UILabel,     :name,      text: 'Seaport World Trade Center'
+        subview UIButton,    :map,       title: 'Map of 200 Seaport Blvd Boston, MA 02210'
+        subview UIImageView, :photo,     image: UIImage.imageNamed('seaport.jpg')
+        subview UIButton,    :book_room, title: 'Book a Room'
+      end
+    end
   end
 end
---
+++ app/styles/location_styles.rb
@@ -0,0 +1,16 @@
+Teacup::Stylesheet.new :location_styles do
+
+  style :name,
+    frame: [[0,0], ["100%", 22]],
+    font: UIFont.boldSystemFontOfSize(14)
+
+  style :map,
+    frame: [[0,22], ["100%", 22]]
+
+  style :photo,
+    frame: [[0,44], [307, 142]]
+
+  style :book_room,
+    frame: [[0,186], ["100%", 22]]
+
+end
