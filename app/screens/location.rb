3ab5018770c4fa38c64bf345f5c549f6230cc56b
Alex Rothenberg <alex@alexrothenberg.com>
2013-09-27 16:53:42 -0400

    location screen with a map

--- app/screens/location.rb
+++ app/screens/location.rb
@@ -1,24 +1,59 @@
 module Screen
-  class Location < PM::Screen
+  class Location < PM::MapScreen
     def will_appear
       mm_drawerController.title = 'Location'
     end
 
+    # start_position latitude: 42.351016, longitude: -71.040786, radius: 1
+    start_position latitude: 42.350284, longitude: -71.048026, radius: 1
+
     stylesheet :location_styles
 
-    def will_present
-      layout(view, :main_view) do
-        subview UILabel,     :name,           text: 'Seaport World Trade Center'
-        subview UIButton,    :address,        title: '200 Seaport Blvd Boston, MA 02210'
-        @map = subview UIButton, :map_button, title: 'Show Map'
-        subview UIImageView, :photo,          image: UIImage.imageNamed('seaport.jpg')
-        subview UIButton,    :book_room,      title: 'Book a Room'
-      end
+    def screen_setup
+      super
 
-      @map.when_tapped { load_map }
+      layout(view, :main_view) do
+        @copy = subview(UILabel, :copy, text: caption, numberOfLines: 0)
+        @links = subview(UIView, :links) do
+          subview(UIButton.buttonWithType(UIButtonTypeRoundedRect), :hotel,       title: 'http://seaportboston.com').when(UIControlEventTouchUpInside) do
+            App.open_url 'http://seaportboston.com'
+          end
+
+          subview(UIButton.buttonWithType(UIButtonTypeRoundedRect), :book_room, title: 'Book a Room at special rate').when(UIControlEventTouchUpInside) do
+            App.open_url 'https://book.b4checkin.com/seaportboston/rlpv2/NegotiatedRates.asp?TID=DOCK13&CompanyName=Wicked%20Good%20Ruby%20Conference&Category=Group'
+          end
+        end
+      end
+      map.height = view.height - 190
+      @copy.top = map.bottom
+      @copy.height = 190
+      @copy.sizeToFit
+      @links.top = @copy.bottom
     end
 
-    def load_map
+    def annotation_data
+      @data ||= [{
+          latitude: 42.351016,
+          longitude: -71.040786,
+          title: "Seaport World Trade Center",
+          subtitle: "Wicked Good Ruby Here"
+        },
+        {
+          latitude: 42.3522716,
+          longitude: -71.055242,
+          title: "South Station",
+          subtitle: "Trains"
+        }
+      ]
+    end
+
+    def caption
+"Wicked Good Ruby Conf is going to be held at the Seaport Hotel & World Trade Center in Boston.
+
+We recommend staying at their hotel if you are coming from out of town. We have reserved a number of single and double rooms for conference attendees."
+    end
+
+    def load_url
       App.open_url 'http://maps.apple.com/?ll=42.351016,-71.040786&spn=0.01&q=44+Commonwealth+Pier+Boston%2C+MA+02210'
     end
   end
--- app/styles/location_styles.rb
+++ app/styles/location_styles.rb
@@ -1,19 +1,20 @@
 Teacup::Stylesheet.new :location_styles do
 
-  style :name,
-    frame: [[0,0], ["100%", 22]],
-    font: UIFont.boldSystemFontOfSize(14)
+  style :main_view,
+    backgroundColor: UIColor.whiteColor
 
-  style :address,
-    frame: [[0,200], ["100%", 22]]
+  style :copy,
+    frame: [[8,0], ["100% - 16", "100%"]],
+    font: UIFont.systemFontOfSize(14),
+    backgroundColor: UIColor.whiteColor
 
-  style :map_button,
-    frame: [[0,230], ["100%", 22]]
+  style :links,
+    frame: [[8,0], ["100%", 44]]
 
-  style :photo,
-    frame: [[0,44], [307, 142]]
+  style :hotel,
+    frame: [[0,0], ["100%", 22]]
 
   style :book_room,
-    frame: [[0,186], ["100%", 22]]
+    frame: [[0,22], ["100%", 22]]
 
 end
