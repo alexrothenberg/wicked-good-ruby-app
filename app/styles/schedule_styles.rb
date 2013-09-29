3122c1a50dac12915c6a451b0591ced5ab2caf47
Alex Rothenberg <alex@alexrothenberg.com>
2013-09-29 18:14:04 -0400

    removed constraints from styles for performance

--- app/styles/schedule_styles.rb
+++ app/styles/schedule_styles.rb
@@ -7,68 +7,25 @@ Teacup::Stylesheet.new :schedule_styles do
     backgroundColor: UIColor.whiteColor,
     font: UIFont.systemFontOfSize(10)
 
-  style :day_selector,
-    constraints: [
-      :top_left,
-      :full_width,
-      constrain_height(44)
-    ]
-
   TALK_ROW_HEIGHT = 44
-  style :talks_table,
-    constraints: [
-      constrain_left(0),
-      :full_width,
-      constrain(:height).equals(:superview, :height),
-      constrain_below(:day_selector)
-    ]
-
   style :time,
-    constraints: [
-      constrain_top(0),
-      constrain_left(HORIZ_MARGIN),
-      constrain_width(50),
-      constrain_height(TALK_ROW_HEIGHT)
-    ]
+    frame: [[HORIZ_MARGIN, 0], [50, TALK_ROW_HEIGHT]]
 
   style :talk_1,
-    constraints: [
-      constrain_top(0),
-      constrain_left(55),
-      constrain(:width).equals(:superview, :width).minus(60),
-      constrain_height(TALK_ROW_HEIGHT)
-    ]
+    frame: [[40, 0], ["100% - 55", TALK_ROW_HEIGHT]]
 
   style :talk_2,
-    constraints: [
-      constrain_left(55),
-      constrain(:width).equals(:superview, :width).minus(60),
-      constrain_height(TALK_ROW_HEIGHT),
-      constrain_below(:talk_1)
-    ]
-
-  style :title,
-  font: UIFont.boldSystemFontOfSize(10),
-    constraints: [
-      :top_left,
-      constrain(:width).equals(:superview, :width).minus(65),
-      :full_height
-    ]
-
-  style :location,
-    constraints: [
-      :top_right,
-      constrain_width(65),
-      :full_height
-    ]
+    frame: [[40, TALK_ROW_HEIGHT + 8], ["100% - 65", TALK_ROW_HEIGHT]]
 
   style :ruby_image,
-    constraints: [
-      constrain(:self, :center_y).equals(:title, :center_y),
-      constrain_to_left(:title).minus(4),
-      constrain_height(10),
-      constrain_width(10)
-    ]
+    frame: [[0, (TALK_ROW_HEIGHT/2) - 5], [10, 10]]
+
+  style :title,
+    font: UIFont.boldSystemFontOfSize(10),
+    frame: [[14, 0], ["100% - 70", "100%"]]
+
+  style :location,
+    frame: [[200, 0], [65, "100%"]]
 
 
 end
